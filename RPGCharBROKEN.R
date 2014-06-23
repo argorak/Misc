1f0f3fa2cb6aacc48993ac3e4c2c6d158079ae6b
argorak <judyma@mymail.vcu.edu>
2014-06-22 21:03:50 -0400

    Add BROKEN example
    Note the funk HP behavior.

---
+++ RPGCharBROKEN.R
@@ -0,0 +1,21 @@
+## This file creates a simple RPG character in R.
+
+makeCharacter <- function(hpMAX, atk, def, name) {
+    hp <- hpMAX
+    ## getter functions
+    gethp <- function() hp
+    gethpMAX <- function() hpMAX
+    getatk <- function() atk
+    getdef <- function() def
+    getname <- function() name
+    ## setter functions
+    sethp <- function(x) hp <<- x
+    sethpMAX <- function(x) hpMAX <<- x
+    setatk <- function(x) atk <<- x
+    setdef <- function(x) def <<- x
+    setname <- function(x) name <<- x
+    ## list of functions
+    list(gethp = gethp, gethpMAX = gethpMAX, getatk = getatk, getdef = getdef,
+         sethp = sethp, sethpMAX = sethpMAX, setatk = setatk, setdef = setdef,
+         getname = getname, setname = setname)
+}
